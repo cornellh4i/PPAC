f23cf1aad2f0c7898e2e29f22610b786e38b9f7f
Samantha Cruz <samantha.jaycruz@gmail.com>
2026-02-11 20:32:08 -0500

    model done

--- backend/src/users/models.ts
+++ backend/src/users/models.ts
@@ -1,6 +1,28 @@
 import { getModelForClass, prop } from "@typegoose/typegoose";
+import { ObjectId } from "mongoose";
 
 class User {
+    constructor(_id: ObjectId, firebaseUid: string, email: string, name: string, createdAt: Date, role: ("student" | "practitioner" | "admin"), profileImage: string, practitionerDetails?: {specialty: string, organization: string, location: string, verified: boolean}) {
+    this._id = _id;
+    this.firebaseUid = firebaseUid;
+    this.email = email;
+    this.name = name;
+    this.createdAt = createdAt;
+    this.role = role;
+    if (role === "practitioner") {
+        this.practitionerDetails = {
+            specialty: practitionerDetails?.specialty || "",
+            organization: practitionerDetails?.organization || "",
+            location: practitionerDetails?.location || "",
+            verified: practitionerDetails?.verified || false
+        };
+    }
+
+  }
+
+    @prop()
+    public _id!: ObjectId;
+
     @prop({ required: true, unique: true })
     public firebaseUid!: string;
 
@@ -12,6 +34,20 @@ class User {
 
     @prop({ default: Date.now })
     public createdAt?: Date;
+
+    @prop()
+    public role!: "student" | "practitioner" | "admin";
+
+    @prop()
+    public profileImage?: string;
+
+    @prop()
+    public practitionerDetails?: {
+        specialty: string;
+        organization: string;
+        location: string;
+        verified: boolean;
+    };
 }
 
 
