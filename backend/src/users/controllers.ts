36a93aa9bb8384ae53604380adb985a5a7aef5a0
Samantha Cruz <samantha.jaycruz@gmail.com>
2026-02-18 17:04:22 -0500

    Flipped .find and .findOne

--- backend/src/users/controllers.ts
+++ backend/src/users/controllers.ts
@@ -1,9 +1,9 @@
 import { User, UserModel } from "./models";
 import mongoose from "mongoose";
 
-const getUsers = async () => UserModel.findOne({});
+const getUsers = async () => UserModel.find({});
 
-const getUserById = async (id: mongoose.Types.ObjectId) => UserModel.find({ _id: id });
+const getUserById = async (id: mongoose.Types.ObjectId) => UserModel.findOne({ _id: id });
 
 const updateUser = async (id: mongoose.Types.ObjectId, updatedFields: Partial<User>) =>
   UserModel.findOneAndUpdate({ _id: id }, updatedFields, { new: true });
