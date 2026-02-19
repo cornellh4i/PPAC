69035c372dd51e52b9b116f305eed8e1de334192
Samantha Cruz <samantha.jaycruz@gmail.com>
2026-02-18 20:04:12 -0500

    gets and Update complete

--- backend/src/resources/controllers.ts
+++ backend/src/resources/controllers.ts
@@ -1,13 +1,14 @@
 import { Resource, ResourceModel } from "./models";
+import mongoose from "mongoose";
 
 const addResource = async (resource: Resource) => {
   const newResource = new ResourceModel(resource);
   return newResource.save();
 }
 const getResources = async () => ResourceModel.find({});
-const getResourceById = async (id: string) => ResourceModel.findOne({ _id: id });
-const updateResource = async (id: string, updatedFields: Partial<Resource>) => ResourceModel.findOneAndUpdate({ _id: id }, updatedFields);   
-const deleteResourceById = async (id: string) => ResourceModel.deleteOne({ _id: id });
+const getResourceById = async (id: mongoose.Types.ObjectId) => ResourceModel.findOne({ _id: id });
+const updateResource = async (id: mongoose.Types.ObjectId, updatedFields: Partial<Resource>) => ResourceModel.findOneAndUpdate({ _id: id }, updatedFields);   
+const deleteResourceById = async (id: mongoose.Types.ObjectId) => ResourceModel.deleteOne({ _id: id });
 
 export default {
     addResource,
---
+++ backend/src/resources/views.ts
@@ -0,0 +1,45 @@
+import { Router, Request } from "express";
+import resourceControllers from "./controllers";
+import mongoose from "mongoose";
+import { successJson, errorJson } from "../utils/jsonResponses";
+
+const resourceRouter = Router();
+
+resourceRouter.get("/", async (req, res) => {
+  try {
+    res.status(200).send(await resourceControllers.getResources());
+  } catch (error) {
+    res.status(400).send(errorJson("Failed to fetch resources"));
+  }
+});
+
+resourceRouter.get("/:id", async (req: Request<{ id: string }>, res) => {
+  try {
+    const id = new mongoose.Types.ObjectId(req.params.id);
+    const resource = await resourceControllers.getResourceById(id);
+    if(resource.length > 0){
+      res.status(200).send(successJson(resource));
+    } else {
+      res.status(404).send(errorJson("Resource not found"));
+    }
+  } catch (error) {
+    res.status(400).send(errorJson("Invalid resource ID"));
+  }
+});
+
+resourceRouter.put("/:id", async (req: Request<{ id: string }>, res) => {
+  try {
+    const id = new mongoose.Types.ObjectId(req.params.id);
+    const resource = await resourceControllers.getResourceById(id);
+    if (!resource) {
+      res.status(404).send(errorJson("Resource not found"));
+      return;
+    }
+    const updatedResource = await resourceControllers.updateResource(id, req.body);
+    res.status(200).send(successJson(updatedResource));
+  } catch (error) {
+    res.status(400).send(errorJson("Failed to update resource"));
+  }
+});
+
+export default resourceRouter;
