9d9762598a5b8bc326993a49547ba323f1dd7563
Samantha Cruz <samantha.jaycruz@gmail.com>
2026-03-11 03:56:40 -0400

    basic

---
+++ frontend/src/components/atoms/Button/Button.jsx
@@ -0,0 +1,20 @@
+import "./Button.scss";
+
+function Button({
+  children,
+  onClick,
+  variant = "primary",
+  disabled = false,
+}) {
+  return (
+    <button
+      className={`button button--${variant}`}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default Button;
