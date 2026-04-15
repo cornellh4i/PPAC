ab13a4dc162d228abfdd5394ce8a126490709683
Ayan <ak2668@cornell.edu>
2026-04-15 17:57:58 -0400

    Implemented ResourceCard according to Figma designs

---
+++ frontend/src/components/molecules/ResourceCard/ResourceCard.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import './ResourceCard.scss';
+
+interface ResourceCardProps {
+  type: string;
+  tag: string;
+  title: string;
+  file: string;
+  description: string;
+}
+
+const ResourceCard: React.FC<ResourceCardProps> = ({
+  type,
+  tag,
+  title,
+  file,
+  description,
+}) => {
+  return (
+    <article className="resource-card">
+      <div className="resource-card__pill">{type}</div>
+
+      <div className="resource-card__image-wrapper">
+        <img
+          className="resource-card__image"
+          src={file}
+          alt={title}
+        />
+      </div>
+
+      <div className="resource-card__body">
+        <div className="resource-card__copy">
+          <h2 className="resource-card__title">{title}</h2>
+          <p className="resource-card__description">{description}</p>
+        </div>
+
+        <div className="resource-card__footer">
+          <div className="resource-card__pill">{tag}</div>
+
+          <div className="resource-card__link">
+            <span>View details</span>
+            <span className="resource-card__arrow" aria-hidden="true" />
+          </div>
+        </div>
+      </div>
+    </article>
+  );
+};
+
+export default ResourceCard;
--- frontend/src/components/pages/resources/index.tsx
+++ frontend/src/components/pages/resources/index.tsx
@@ -1,11 +1,20 @@
+import ResourceCard from '../../molecules/ResourceCard/ResourceCard';
+
+const resourceTest = {
+  type: 'Guide',
+  tag: '5-min read',
+  title: 'Understand Pelvic Health',
+  file: 'https://www.figma.com/api/mcp/asset/5ed7d8bc-3ff1-4823-9e1b-434e52565ee0',
+  description:
+    'my name is retep and i am evil',
+};
+
 const Resources: React.FC = () => {
-
-    return (
-        <div className="resources">
-            <p>🩺</p>
-        </div>
-    )
-
-}
+  return (
+    <div className="resources">
+      <ResourceCard {...resourceTest} />
+    </div>
+  );
+};
 
 export default Resources;
